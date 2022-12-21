169ed11a0b2939726c2439a1ed7c9d838bb82917
Nima Zare <nima13zare@gmail.com>
2022-12-21 10:30:09 +0330

    skaffold & ingress srv added

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,10 @@ const app = express();
 
 app.use(express.json());
 
+app.get("/api/users/currentuser", (req, res) => {
+  res.send("Hi!");
+});
+
 app.listen(3000, () => {
-  console.log("listening on port 3000!");
+  console.log("listening on port 3000!!");
 });
