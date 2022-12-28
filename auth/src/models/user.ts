da1999df3313e1a52386fd813c93701a3d26f85d
Nima Zare <nima13zare@gmail.com>
2022-12-28 10:26:02 +0330

    hash pass logic

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { passwordHasher } from "../services/password";
 
 interface UserAttrs {
   email: string;
@@ -23,6 +24,14 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+});
+
+userSchema.pre("save", async function (done) {
+  if (this.isModified("password")) {
+    const hashedPassword = await passwordHasher(this.get("password"));
+    this.set("password", hashedPassword);
+  }
+  done();
 });
 
 userSchema.statics.build = (attrs: UserAttrs) => {
