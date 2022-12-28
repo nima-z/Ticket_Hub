acd5fc5d0da5a02b61c44a7048e3bb665a399789
Nima Zare <nima13zare@gmail.com>
2022-12-28 22:42:02 +0330

    cookie session installed

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import "express-async-errors";
 import mongoose from "mongoose";
+import cookieSession from "cookie-session";
 //===========================================================
 import { currentUserRouter } from "./routes/current-user";
 import { signInRouter } from "./routes/signin";
@@ -11,8 +12,15 @@ import { NotFoundError } from "./errors/not-found-error";
 //===========================================================
 
 const app = express();
+app.set("trust proxy", true);
 
 app.use(express.json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
 
 app.use(currentUserRouter);
 app.use(signUpRouter);
