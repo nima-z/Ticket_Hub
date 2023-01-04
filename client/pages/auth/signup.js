bdc7ce84cc24976a674a19c8bb86a9adabe66293
Nima Zare <nima13zare@gmail.com>
2023-01-04 10:22:12 +0330

    error handling on signup error

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,17 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 export default function signup() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([]);
+
+  function emailHandle(e) {
+    setEmail(e.target.value);
+  }
+  function passwordHandle(e) {
+    setPassword(e.target.value);
+  }
+
+  async function onSubmit(event) {
+    event.preventDefault();
+
+    try {
+      const response = await axios.post("/api/users/signup", {
+        email,
+        password,
+      });
+
+      console.log(response.data);
+    } catch (err) {
+      setErrors(err.response.data.errors);
+    }
+  }
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <h1>Sign Up</h1>
       <div className="form-group">
         <label htmlFor="email">Email</label>
-        <input type="text" className="form-control" name="email" />
+        <input
+          type="text"
+          className="form-control"
+          name="email"
+          value={email}
+          onChange={emailHandle}
+        />
       </div>
       <div className="form-group">
         <label htmlFor="password">Password</label>
-        <input type="password" className="form-control" name="password" />
+        <input
+          type="password"
+          className="form-control"
+          name="password"
+          value={password}
+          onChange={passwordHandle}
+        />
       </div>
+      {errors && (
+        <div className="alert alert-danger">
+          <ul className="my-0">
+            {errors.map((error) => (
+              <li key={error.message}>{error.message}</li>
+            ))}
+          </ul>
+        </div>
+      )}
       <button className="btn btn-primary">Sign Up</button>
     </form>
   );
