977d8aea9852924573358ebf956a7cb763c428df
Nima Zare <nima13zare@gmail.com>
2023-01-06 11:02:39 +0330

    initial props on homepage

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,14 @@
-import React from "react";
-
-export default function index() {
+import axios from "axios";
+export default function Home({ currentUser }) {
+  console.log(currentUser);
   return <div>Wellcome Home </div>;
 }
+
+Home.getInitialProps = async () => {
+  if (typeof window === "undefiend") {
+    // so we are on the server
+  } else {
+    //so we are on the browser
+  }
+  return {};
+};
