9b3fd0bd943db3a852c890eff4576073a40f1921
Nima Zare <nima13zare@gmail.com>
2023-01-08 10:26:57 +0330

    npm common module installed in auth

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -3,9 +3,8 @@ import { body } from "express-validator";
 import jwt from "jsonwebtoken";
 //============================================================================
 import { User } from "../models/user";
-import { BadRequestError } from "../errors/bad-request-error";
+import { BadRequestError, validateRequest } from "@nztickethub/common";
 import { comparePassword } from "../services/password";
-import { validateRequest } from "../middlewares/validate-request";
 //============================================================================
 
 const router = express.Router();
