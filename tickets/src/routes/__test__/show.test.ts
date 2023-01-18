1cbc0faf600e137277f2cbb51ff57357d20c2ac7
Nima Zare <nima13zare@gmail.com>
2023-01-18 13:18:05 +0330

    tests for ticket route

---
+++ tickets/src/routes/__test__/show.test.ts
@@ -0,0 +1,25 @@
+import request from "supertest";
+import { app } from "../../app";
+
+it("returns a 404 if the ticket is not found", async () => {
+  await request(app).get("/api/tickets/fkgekguhfkgjhn").send({}).expect(404);
+});
+
+it("returns the ticket if the ticket is found", async () => {
+  const title = "Dream Live Show";
+  const price = 45;
+
+  const res = await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({ title, price })
+    .expect(201);
+
+  const ticketRes = await request(app)
+    .get(`/api/tickets/${res.body.id}`)
+    .send({})
+    .expect(200);
+
+  expect(ticketRes.body.title).toEqual(title);
+  expect(ticketRes.body.price).toEqual(price);
+});
