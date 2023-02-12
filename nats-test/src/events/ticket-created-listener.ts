3f735dc3ab53d7d3653ab41c34cc865af6ea1b46
Nima Zare <nima13zare@gmail.com>
2023-02-12 19:28:08 +0330

    common module updated with events

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -1,12 +1,16 @@
 import { Listener } from "./base_listener";
 import { Message } from "node-nats-streaming";
+import { TicketCreatedEvent } from "./ticket-created-event";
+import { Subjects } from "./subjects";
 
-export class TicketCreatedListener extends Listener {
-  subject = "ticket:created";
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  subject: Subjects.ticketCreated = Subjects.ticketCreated;
   queueGroupName = "payment-service";
 
-  onMessage(data: any, msg: Message) {
+  onMessage(data: TicketCreatedEvent["data"], msg: Message) {
     console.log("Event Data: ", data);
+
+    console.log(data.price);
 
     msg.ack();
   }
