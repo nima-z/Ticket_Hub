2db7242f930362fb8be4606697e5a856e6522899
Nima Zare <nima13zare@gmail.com>
2023-02-13 13:41:20 +0330

    ticket updated publisher on tickets directory

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { natsWrapper } from "./nats-wrapper";
 //===========================================================
 import { app } from "./app";
 //===========================================================
@@ -13,6 +14,16 @@ async function startServer() {
   }
 
   try {
+    await natsWrapper.connect("tickethub", "fdg24", "http://nats-srv:4222");
+
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection is closed");
+      process.exit();
+    });
+
+    process.on("SIGINT", () => natsWrapper.client.close());
+    process.on("SIGTERM", () => natsWrapper.client.close());
+
     await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to Database");
   } catch (err) {
