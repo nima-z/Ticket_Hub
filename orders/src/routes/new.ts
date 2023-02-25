05fd2f03160fba07f3fd3bd08d80ab380dc9766b
Nima Zare <nima13zare@gmail.com>
2023-02-25 10:06:11 +0330

    order logic and tests

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -13,6 +13,8 @@ import { Order } from "../models/order";
 
 const router = express.Router();
 
+const EXPIRATION_WINDOW_SECONDS = 15 * 60;
+
 router.post(
   "/api/orders",
   requireAuth,
@@ -39,8 +41,22 @@ router.post(
     }
 
     // calculate the expiration ddate for this order
+    const expiration = new Date();
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
 
-    res.send({});
+    // build the order and save it to the database
+    const order = Order.build({
+      userId: req.currentUser!.id,
+      status: OrderStatus.Created,
+      expiresAt: expiration,
+      ticket,
+    });
+
+    await order.save();
+
+    //publish an event that says an order was created
+
+    res.status(201).send(order);
   }
 );
 
